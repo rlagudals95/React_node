d9101dc9f74500d00d7c51125e4d17d6972ab457
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2021-06-13 13:27:24 +0900

    0127

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 //백엔드의 시작점
 //터미널에 node index.js로 테스트 할 수 있다
 const express = require("express"); //npm installexpress로 설치해 리액트 처럼 import
+const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose"); //npm install mongoose --save
+const { User } = require("./models/User"); //회원가입시 필요한 유저 모델형식 가져온다
+
+// bodyParser에 옵션을 줘야한다
+// 클라리언트에서 오는 정보를 일기 위함
+
+//application/x-www-form-0urlencoded 같은 형식을 읽기 위함
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json 타입을 분석해서 가져올 수 있게함
+app.use(bodyParser.json());
+
 mongoose
   .connect(
     // mongoDB에서 만든 데이터베이스에 연결
     "mongodb+srv://dbfudgudals:tkdel12@@!!@youtubeclone.rqkg7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
+    {
+      useNewUrlParser: true, //에러가 안뜨게 써준다
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
   )
   .then(() => {
     //mongoose에서 오는 deprecation 경고를 없애기 위해쓴다
@@ -18,9 +34,30 @@ mongoose
   });
 
 app.get("/", (req, res) => {
+  // 콜백함수로 req와 res
   //터미널에 node index.js > localhost:5000으로 접속하면 "hello world"가 보인다
   res.send("hello world");
 });
 
+// 회원가입시 필요한 정보들을 client에서 받으면
+// 그것들을 데이터 베이스에 넣어준다!
+app.post("/register", (req, res) => {
+  // req.body안에는 json형식으로 데이터가 있다!
+  // 이렇게 json형식으로 읽을 수 있는 이유는 bodyParser덕분!
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    // 회원가입 실패
+    if (err) return res.json({ success: false, err });
+    // 회원가입 성공
+    return res.status(200).json({
+      success: true,
+    });
+  }); // mongoDB에 저장
+});
+
+const port = process.env.PORT || 5000;
+
 // 5000포트가 기본이다
-app.listen(5000);
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
--- server/models/User.js
+++ server/models/User.js
@@ -1,93 +1,152 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const jwt = require('jsonwebtoken');
-const moment = require("moment");
+// 유저관련 데이터를 보관하기
+// 모델이란? schema를 감싸주기 위해
+// schema란? 상품을 예로들면 상품에 대한 정보와 타입을 하나하나 정보들을 지정해준 것
+
+const mongoose = require('mongoose')
+
 
 const userSchema = mongoose.Schema({
     name: {
-        type:String,
-        maxlength:50
+        type: String,
+        maxlength : 50,
     },
     email: {
-        type:String,
-        trim:true,
-        unique: 1 
+        type: String,
+        trim: true, // 띄워쓰기를 없애줌
+        unique: 1 // 고유한 것 겹치지 않는 것
     },
     password: {
         type: String,
-        minglength: 5
+        minlength: 5
     },
     lastname: {
-        type:String,
+        type: String,
         maxlength: 50
     },
-    role : {
-        type:Number,
-        default: 0 
+    role: {
+        type: Number, //관리자 설정
+        default:0
     },
     image: String,
-    token : {
-        type: String,
+    token: { //유효성 검사 위해 필요
+        type: String
     },
-    tokenExp :{
+    tokenExp: { // 토큰 유효기간 지정
         type: Number
     }
 })
+//스키마를 모델로 감싸준다 ('모델이름',스키마)
+const User = mongoose.model('User',userSchema)
+
+module.exports= {User}// 다른데 에서도 쓸 수 있게 exports
 
 
-userSchema.pre('save', function( next ) {
-    var user = this;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+// const saltRounds = 10;
+// const jwt = require('jsonwebtoken');
+// const moment = require("moment");
+
+// const userSchema = mongoose.Schema({
+//     name: {
+//         type:String,
+//         maxlength:50
+//     },
+//     email: {
+//         type:String,
+//         trim:true,
+//         unique: 1 
+//     },
+//     password: {
+//         type: String,
+//         minglength: 5
+//     },
+//     lastname: {
+//         type:String,
+//         maxlength: 50
+//     },
+//     role : {
+//         type:Number,
+//         default: 0 
+//     },
+//     image: String,
+//     token : {
+//         type: String,
+//     },
+//     tokenExp :{
+//         type: Number
+//     }
+// })
+
+
+// userSchema.pre('save', function( next ) {
+//     var user = this;
     
-    if(user.isModified('password')){    
-        // console.log('password changed')
-        bcrypt.genSalt(saltRounds, function(err, salt){
-            if(err) return next(err);
+//     if(user.isModified('password')){    
+//         // console.log('password changed')
+//         bcrypt.genSalt(saltRounds, function(err, salt){
+//             if(err) return next(err);
     
-            bcrypt.hash(user.password, salt, function(err, hash){
-                if(err) return next(err);
-                user.password = hash 
-                next()
-            })
-        })
-    } else {
-        next()
-    }
-});
+//             bcrypt.hash(user.password, salt, function(err, hash){
+//                 if(err) return next(err);
+//                 user.password = hash 
+//                 next()
+//             })
+//         })
+//     } else {
+//         next()
+//     }
+// });
 
-userSchema.methods.comparePassword = function(plainPassword,cb){
-    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-        if (err) return cb(err);
-        cb(null, isMatch)
-    })
-}
+// userSchema.methods.comparePassword = function(plainPassword,cb){
+//     bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+//         if (err) return cb(err);
+//         cb(null, isMatch)
+//     })
+// }
 
-userSchema.methods.generateToken = function(cb) {
-    var user = this;
-    console.log('user',user)
-    console.log('userSchema', userSchema)
-    var token =  jwt.sign(user._id.toHexString(),'secret')
-    var oneHour = moment().add(1, 'hour').valueOf();
+// userSchema.methods.generateToken = function(cb) {
+//     var user = this;
+//     console.log('user',user)
+//     console.log('userSchema', userSchema)
+//     var token =  jwt.sign(user._id.toHexString(),'secret')
+//     var oneHour = moment().add(1, 'hour').valueOf();
 
-    user.tokenExp = oneHour;
-    user.token = token;
-    user.save(function (err, user){
-        if(err) return cb(err)
-        cb(null, user);
-    })
-}
+//     user.tokenExp = oneHour;
+//     user.token = token;
+//     user.save(function (err, user){
+//         if(err) return cb(err)
+//         cb(null, user);
+//     })
+// }
 
-userSchema.statics.findByToken = function (token, cb) {
-    var user = this;
+// userSchema.statics.findByToken = function (token, cb) {
+//     var user = this;
 
-    jwt.verify(token,'secret',function(err, decode){
-        user.findOne({"_id":decode, "token":token}, function(err, user){
-            if(err) return cb(err);
-            cb(null, user);
-        })
-    })
-}
+//     jwt.verify(token,'secret',function(err, decode){
+//         user.findOne({"_id":decode, "token":token}, function(err, user){
+//             if(err) return cb(err);
+//             cb(null, user);
+//         })
+//     })
+// }
 
-const User = mongoose.model('User', userSchema);
+// const User = mongoose.model('User', userSchema);
 
-module.exports = { User }
+// module.exports = { User }
